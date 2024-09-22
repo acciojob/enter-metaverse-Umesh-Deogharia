09f277dafcb4e01e06a3c7795e72e221d7afbc4d
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-22 11:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let Status = document.getElementById('status');
+let btn = document.getElementById('enterBtn');
+
+btn.addEventListener('click',()=>{
+	Status.innerHTML = '<h1>Enter the Metaverse</h1>'
+})
